b4e6f4801a5e258efe512a3ba82fc59ced522c48
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-11-10 02:21:18 +0530

    added video card ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,20 @@ import { auth } from "../firebase";
 import { authContext } from "../AuthProvider";
 import { useContext } from "react";
 import { Redirect } from "react-router";
+import "./home.css";
+import VideoCard from "./videoCard";
 
 let Home = () => {
 	let user = useContext(authContext);
 	return (
 		<>
 			{user ? "" : <Redirect to="/login" />}
-			<h1>Home</h1>
+			<div className="video-container">
+				<VideoCard />
+				
+			</div>
 			<button
+				className="home-logout-btn"
 				onClick={() => {
 					// jo bhi method se signin kara usko call karke logout kardega/signout ho jaega
 					auth.signOut();
