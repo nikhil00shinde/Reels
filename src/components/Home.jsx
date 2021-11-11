558b8f49f7b1c528cc0494764301611c937ada28
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-11-11 18:17:26 +0530

    upload functionality added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { auth } from "../firebase";
+import { auth, storage } from "../firebase";
 import { authContext } from "../AuthProvider";
 import { useContext } from "react";
 import { Redirect } from "react-router";
@@ -12,7 +12,6 @@ let Home = () => {
 			{user ? "" : <Redirect to="/login" />}
 			<div className="video-container">
 				<VideoCard />
-				
 			</div>
 			<button
 				className="home-logout-btn"
@@ -23,6 +22,43 @@ let Home = () => {
 			>
 				Logout
 			</button>
+			<input
+				type="file"
+				onChange={(e) => {
+					let videoObj = e.currentTarget.files[0];
+
+					let { name, type, size } = videoObj;
+
+					size = size / 1000000;
+
+					if (size > 10) {
+						alert("file size exceeds 10mb");
+						return;
+					}
+
+					type = type.split("/")[0];
+
+					if (type !== "video") {
+						alert("Please upload a video file");
+						return;
+					}
+
+					//gives a event
+					let uploadTask = storage
+						.ref(`/posts/${user.uid}/${Date.now() + "-" + name}`)
+						.put(videoObj);
+
+					//uploadTask is event we apply listener to it
+					uploadTask.on("state_changed", null, null, () => {
+						// jb upload ho jati file hamare storage pe
+						// tab ye function chalta hain
+
+						uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+							console.log(url);
+						});
+					});
+				}}
+			/>
 		</>
 	);
 };
--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,7 @@ import config from "./config.json";
 
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 firebase.initializeApp(config);
 
@@ -13,11 +14,11 @@ let provider = new firebase.auth.GoogleAuthProvider();
 // object jiske ander login/logout/signup ki functionality hain
 export const auth = firebase.auth();
 
-
 // getting object of firestore
 export const firestore = firebase.firestore();
 
-
+//getting storage object
+export const storage = firebase.storage();
 
 export const signInWithGoogle = () => {
 	// ki muje popup ko use krke sign up krna with google
